dfa4484320d5b871478bdc4c19f3060985f6e198
Roland Elliott <elliott.roland@gmail.com>
2014-07-26 01:32:47 +0200

    Add word-spcific pages
    Also, add basic css

--- treasure_house/urls.py
+++ treasure_house/urls.py
@@ -6,7 +6,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'treasure_house.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^words/', include('words.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- words/views.py
+++ words/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from words.models import Word
+
+def index(request):
+  return HttpResponse("Hello, world!")
+
+def definition(request, word_id):
+  context = {"word": get_object_or_404(Word, id=word_id)}
+  return render(request, "words/definition.html", context)
