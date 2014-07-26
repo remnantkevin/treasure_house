2312cbeb78d2af3131ef5d7cde579da65d868892
Roland Elliott <elliott.roland@gmail.com>
2014-07-26 23:57:20 +0200

    Add search functionality

--- words/views.py
+++ words/views.py
@@ -8,15 +8,19 @@ from words.models import Word
 
 # index page of all words
 def index(request):
-  list_of_words = []
-  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  for letter in alpha:
-    letter_list = Word.objects.filter(word__istartswith=letter)
-    if len(letter_list) > 0:
-      list_of_words.append(letter_list)
+  if len(request.GET) == 0:
+    list_of_words = []
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for letter in alpha:
+      letter_list = Word.objects.filter(word__istartswith=letter)
+      if len(letter_list) > 0:
+        list_of_words.append(letter_list)
 
-  context = {"list_of_words": list_of_words}
-  return render(request, "words/index.html", context)
+    context = {"list_of_words": list_of_words}
+    return render(request, "words/index.html", context)
+  else:
+    return render(request, "words/search.html", {"search": request.GET["search"]})
+  
 
 def definition(request, word_id):
   context = {"word": get_object_or_404(Word, id=word_id)}
